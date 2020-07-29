82b6cc5b26d8a73dcc5042d4c65bf6b376ba630d
DouglasMonteles <silvadouglas328@gmail.com>
2020-07-29 16:59:56 -0300

    Definir direcoes e impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const context = canvas.getContext("2d");
 const box = 32;
 
 const snake = [];
-const direction = 'right';
+var direction = 'right';
 
 snake[0] = {
   x: 8 * box,
@@ -22,7 +22,43 @@ function handleSnake() {
   });
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+  if (event.keyCode === 37 && direction !== 'right') { // 37 is the code for left
+    direction = 'left';
+  }
+
+  if (event.keyCode === 38 && direction !== 'up') { // 38 is the code for up
+    direction = 'down';
+  }
+
+  if (event.keyCode === 39 && direction !== 'left') { // 39 is the code for right
+    direction = 'right';
+  }
+
+  if (event.keyCode === 40 && direction !== 'down') { // 40 is the code for down
+    direction = 'up';
+  }
+}
+
 function startGame() {
+  if (snake[0].x > 15 * box && direction === 'right') {
+    snake[0].x = 0;
+  }
+
+  if (snake[0].x < 0 && direction === 'left') {
+    snake[0].x = 16 * box;
+  }
+
+  if (snake[0].y > 15 * box && direction === 'up') {
+    snake[0].y = 0;
+  }
+
+  if (snake[0].y < 0 && direction === 'down') {
+    snake[0].y = 16 * box;
+  }
+
   handleBG();
   handleSnake();
 
