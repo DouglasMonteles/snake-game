bc46275f887427bc79a3020f2d54781908eae747
DouglasMonteles <silvadouglas328@gmail.com>
2020-07-29 14:48:03 -0300

    Criar cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const canvas = document.getElementById('snake');
 const context = canvas.getContext("2d");
 const box = 32;
+const snake = [];
 
-function criarBG() {
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box,
+};
+
+function handleBG() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG();
+function handleSnake() {
+  snake.map(it => {
+    context.fillStyle = 'green';
+    context.fillRect(it.x, it.y, box, box);
+  });
+}
+
+handleBG();
+handleSnake();
